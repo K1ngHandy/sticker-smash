e3bdfcae48d7aa67daa2137755193076ebdf371e
K1ngHandy <bolds-slicks0g@icloud.com>
2025-01-14 02:27:20 -0500

    add navigation

---
+++ app/+not-found.tsx
@@ -0,0 +1,29 @@
+import { View, StyleSheet } from "react-native";
+import { Link, Stack } from "expo-router";
+
+export default function NotFound() {
+    return (
+        <>
+            <Stack.Screen options={{ title: 'Not Found' }} />
+            <View style={styles.container}>
+                <Link href="/" style={styles.button}>
+                    Go Back to Home screen
+                </Link>
+            </View>
+        </>
+    );
+}
+
+const style = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#25292e",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    button: {
+        fontSize: 20,
+        textDecorationLine: "underline",
+        color: "#fff",
+    }
+})
