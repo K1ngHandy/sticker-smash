e091e11abef730c8e3171a9c9a97323e13dd745c
K1ngHandy <bolds-slicks0g@icloud.com>
2025-01-14 19:18:30 -0500

    add image picker

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,4 +1,6 @@
-import { StyleSheet, View } from "react-native";
+import { View, StyleSheet } from "react-native";
+import * as ImagePicker from "expo-image-picker";
+import { useState } from "react";
 
 import Button from "../components/Button";
 import ImageViewer from "../components/ImageViewer";
@@ -6,17 +8,34 @@ import ImageViewer from "../components/ImageViewer";
 const PlaceholderImage = require('@/assets/images/background-image.png');
 
 export default function Index() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.imageContainer}>
-        <ImageViewer imgSource={PlaceholderImage} />
+  const [ selectedImage, setSelectedImage ] = useState<string | undefined>(undefined);
+
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ['images'],
+      allowsEditing: true,
+      quality: 1,
+    });
+
+    if (!result.cancelled) {
+      setSelectedImage(result.assets[0].uri);
+    } else {
+      alert('No image selected');
+    }
+  };
+
+    return (
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} />
+        </View>
+        <View style={styles.footerContainer}>
+          <Button theme="primary" label="Choose photo" onPress={pickImageAsync} />
+          <Button label="Use this photo" />
+        </View>
       </View>
-      <View style={styles.footerContainer}>
-        <Button theme="primary" label="Choose photo" />
-        <Button label="Use this photo" />
-      </View>
-    </View>
-  );
+    );
+  // };
 }
 
 const styles = StyleSheet.create({
